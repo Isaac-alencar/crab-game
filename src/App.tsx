8a3dbda26926f4828a2415747c0d8acc0187747e
Isaac Alencar <alencarisaac3@gmail.com>
2023-12-21 17:39:33 -0300

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ import { GameProvider } from "./contexts/GameContext";
 function App() {
   return (
     <GameProvider>
+      <Header />
       <Game />
     </GameProvider>
   );
 }
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1>Craaaaab Game</h1>
+    </header>
+  );
+};
 
 export default App;
